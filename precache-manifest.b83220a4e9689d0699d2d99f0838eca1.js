4246c27c46d9588d0ad5a85440b5b960a13f86dc
pokumars <ohenebakob@hotmail.com>
2019-06-22 10:55:24 +0300

    Updates

---
+++ precache-manifest.b83220a4e9689d0699d2d99f0838eca1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecd5568232e6040abd424de3ba9ad273",
+    "url": "/pokumars/ghPagesTest/index.html"
+  },
+  {
+    "revision": "1dba3e53fd4ee4180272",
+    "url": "/pokumars/ghPagesTest/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/pokumars/ghPagesTest/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "1dba3e53fd4ee4180272",
+    "url": "/pokumars/ghPagesTest/static/js/main.658ef9d8.chunk.js"
+  },
+  {
+    "revision": "d824850ebac00b07550b",
+    "url": "/pokumars/ghPagesTest/static/js/runtime~main.bb432cfd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pokumars/ghPagesTest/static/media/logo.5d5d9eef.svg"
+  }
+]);
